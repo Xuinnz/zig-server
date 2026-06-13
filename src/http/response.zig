e165cfcba0fd7e4b9d69f7d65bba5a132a76d688
Xuinnz <redtagura07@gmail.com>
2026-06-13 16:27:38 +0800

    Fix: Separate Writer Function for Header

--- src/http/response.zig
+++ src/http/response.zig
@@ -19,6 +19,20 @@ pub fn statusText(code: StatusCode) []const u8 {
     };
 }
 
+// for file serving only, this sends headers, send body separately
+pub fn writeHeaders(
+    writer: anytype,
+    code: StatusCode,
+    content_type: []const u8,
+    content_length: usize,
+) !void {
+    try writer.print("HTTP/1.1 {d} {s}\r\n", .{ @intFromEnum(code), statusText(code) });
+    try writer.print("Content-Type: {s}\r\n", .{content_type});
+    try writer.print("Content-Length: {d}\r\n", .{content_length});
+    try writer.writeAll("Connection: close\r\n");
+    try writer.writeAll("\r\n");
+}
+
 //writer
 pub fn write(
     writer: anytype,
@@ -26,10 +40,6 @@ pub fn write(
     content_type: []const u8,
     body: []const u8,
 ) !void {
-    try writer.print("HTTP/1.1 {d} {s}\r\n", .{ @intFromEnum(code), statusText(code) });
-    try writer.print("Content-Type: {s}\r\n", .{content_type});
-    try writer.print("Content-Length: {d}\r\n", .{body.len});
-    try writer.writeAll("Connection: close\r\n");
-    try writer.writeAll("\r\n");
+    try writeHeaders(writer, code, content_type, body.len);
     try writer.writeAll(body);
 }
