66ce59aecfb1cf20814b9cef507816e018187134
Xuinnz <redtagura07@gmail.com>
2026-06-13 16:26:12 +0800

    Feat: Added Media Types

---
+++ src/http/mime.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn fromExtension(ext: []const u8) []const u8 {
+    if (std.mem.eql(u8, ext, ".html")) return "text/html";
+    if (std.mem.eql(u8, ext, ".css")) return "text/css";
+    if (std.mem.eql(u8, ext, ".js")) return "application/javascript";
+    if (std.mem.eql(u8, ext, ".png")) return "image/png";
+    if (std.mem.eql(u8, ext, ".jpg")) return "image/jpeg";
+    if (std.mem.eql(u8, ext, ".ico")) return "image/x-icon";
+    return "application/octet-stream";
+}
