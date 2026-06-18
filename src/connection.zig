a06e5ce060db2335c83e409c0f556c334e979322
Xuinnz <redtagura07@gmail.com>
2026-06-19 00:36:38 +0800

    feat: passing routing instance

--- src/connection.zig
+++ src/connection.zig
@@ -3,6 +3,7 @@ const posix = std.posix;
 const parser = @import("http/parser.zig");
 const response = @import("http/response.zig");
 const mime = @import("http/mime.zig");
+const router = @import("router.zig");
 
 //connection phases
 pub const Phase = enum {
@@ -32,14 +33,17 @@ pub const Connection = struct {
     }
 };
 
-pub fn handleEvent(conn: *Connection, allocator: std.mem.Allocator) !bool {
+pub fn handleEvent(conn: *Connection, allocator: std.mem.Allocator, r: *const router.Router) !bool {
     switch (conn.phase) {
         .reading_headers => {
             //loop to get all data
             while (true) {
                 //buffer full (8192)
                 if (conn.total_read == conn.buffer.len) {
-                    try sendError(conn.fd, .request_header_fields_too_large);
+                    var buf: [128]u8 = undefined;
+                    var fbs = std.io.fixedBufferStream(&buf);
+                    try response.write(fbs.writer(), .request_header_fields_too_large, "text/plain", "Too Large");
+                    _ = try posix.send(conn.fd, fbs.getWritten(), 0);
                     return true;
                 }
                 //receive data
@@ -61,15 +65,16 @@ pub fn handleEvent(conn: *Connection, allocator: std.mem.Allocator) !bool {
                     const req_line = data[0..req_line_end];
 
                     const request = parser.parseRequestLine(req_line) catch {
-                        try sendError(conn.fd, .bad_request);
+                        var buf: [128]u8 = undefined;
+                        var fbs = std.io.fixedBufferStream(&buf);
+                        try response.write(fbs.writer(), .bad_request, "text/plain", "Bad Request");
+                        _ = try posix.send(conn.fd, fbs.getWritten(), 0);
                         return true;
                     };
 
                     std.debug.print("{s} {s}\n", .{ request.method, request.path });
-
-                    //req path, will move to routing soon
-                    const path = if (std.mem.eql(u8, request.path, "/")) "/index.html" else request.path;
-                    try serveFile(conn.fd, path, allocator);
+                    //after getting the header, we load the route.
+                    _ = try r.dispatch(request.method, request.path, conn.fd, allocator);
                     return true;
                 }
             }
@@ -80,48 +85,4 @@ pub fn handleEvent(conn: *Connection, allocator: std.mem.Allocator) !bool {
         //connection finished.
         .done => return true,
     }
-}
-
-fn sendError(fd: posix.fd_t, code: response.StatusCode) !void {
-    var buf: [256]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&buf);
-    try response.write(fbs.writer(), code, "text/plain", response.statusText(code));
-    _ = try posix.send(fd, fbs.getWritten(), 0);
-}
-
-fn sendStatic(fd: posix.fd_t, body: []const u8) !void {
-    //TODO: this is 4KB limit, stream the file in chunks directly to posix.send
-    var buf: [4096]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&buf);
-    try response.write(fbs.writer(), .ok, "text/plain", body);
-    _ = try posix.send(fd, fbs.getWritten(), 0);
-}
-
-fn serveFile(fd: posix.fd_t, path: []const u8, allocator: std.mem.Allocator) !void {
-    var file_path_buf: [512]u8 = undefined;
-    //concat public to file path so they can only access these.
-    const file_path = try std.fmt.bufPrint(&file_path_buf, "public{s}", .{path});
-
-    //open and read the file
-    const file = std.fs.cwd().openFile(file_path, .{}) catch {
-        try sendError(fd, .not_found);
-        return;
-    };
-    defer file.close();
-
-    //file content load into the allocator, currently hard capped of 1MB
-    const body = try file.readToEndAlloc(allocator, 1024 * 1024);
-    defer allocator.free(body);
-
-    //extract file extention
-    const ext = std.fs.path.extension(file_path);
-    const content_type = mime.fromExtension(ext);
-
-    var header_buf: [512]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&header_buf);
-    try response.writeHeaders(fbs.writer(), .ok, content_type, body.len);
-    _ = try posix.send(fd, fbs.getWritten(), 0);
-
-    // body
-    _ = try posix.send(fd, body, 0);
 }
