a9890fe32b5248cebe9c4d3a74907028aec3a83c
Xuinnz <redtagura07@gmail.com>
2026-06-24 09:38:30 +0800

    feat: added pdf media type

--- src/http/mime.zig
+++ src/http/mime.zig
@@ -7,5 +7,6 @@ pub fn fromExtension(ext: []const u8) []const u8 {
     if (std.mem.eql(u8, ext, ".png")) return "image/png";
     if (std.mem.eql(u8, ext, ".jpg")) return "image/jpeg";
     if (std.mem.eql(u8, ext, ".ico")) return "image/x-icon";
+    if (std.mem.eql(u8, ext, ".pdf")) return "application/pdf";
     return "application/octet-stream";
 }
