b1b4d1f86c5b195c114b25ab1e2ae2ab87e98993
Xuinnz <redtagura07@gmail.com>
2026-06-27 14:30:44 +0800

    added media type for wasm

--- src/http/mime.zig
+++ src/http/mime.zig
@@ -8,5 +8,6 @@ pub fn fromExtension(ext: []const u8) []const u8 {
     if (std.mem.eql(u8, ext, ".jpg")) return "image/jpeg";
     if (std.mem.eql(u8, ext, ".ico")) return "image/x-icon";
     if (std.mem.eql(u8, ext, ".pdf")) return "application/pdf";
+    if (std.mem.eql(u8, ext, ".wasm")) return "application/wasm";
     return "application/octet-stream";
 }
